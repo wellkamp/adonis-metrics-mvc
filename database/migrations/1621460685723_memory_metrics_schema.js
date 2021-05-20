2b790f851e3b4d06af125fda9b5d2947b3a5fb3f
wellkamp <wellkamp@gmail.com>
2021-05-20 09:57:07 -0300

    add: memory migration

---
+++ database/migrations/1621460685723_memory_metrics_schema.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use("Schema");
+
+class MemoryMetricsSchema extends Schema {
+    up() {
+        this.create("memory_metrics", (table) => {
+            table.increments();
+            table.float("used").notNullable();
+            table.float("available").notNullable();
+            table.integer("pc_id").unsigned().index();
+            table.timestamps();
+        });
+    }
+
+    down() {
+        this.drop("memory_metrics");
+    }
+}
+
+module.exports = MemoryMetricsSchema;
