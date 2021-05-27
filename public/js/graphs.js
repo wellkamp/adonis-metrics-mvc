c997ea638b56052f8b2bf72cafbf70bdb06023d1
wellkamp <wellkamp@gmail.com>
2021-05-27 17:03:55 -0300

    fix: front

--- public/js/graphs.js
+++ public/js/graphs.js
@@ -12,19 +12,41 @@ if (!gpuValues.lenght) {
                     label: "Últimas temperaturas da GPU CORE",
                     data: temperatures,
                     fill: false,
+                    color: "rgba(255,255,255, 0.5)",
+                    backgroundColor: "rgba(255,255,255, 0.5)",
+                    borderWidth: 1,
                     borderColor: "#9400D3",
                     tension: 0.1,
                 },
             ],
         },
         options: {
+            legend: {
+                labels: {
+                    fontColor: "rgba(255,255,255, 0.5)",
+                },
+            },
             scales: {
                 yAxes: [
                     {
                         ticks: {
+                            fontColor: "rgba(255,255,255, 0.5)",
                             beginAtZero: true,
                             max: 100,
                             min: 0,
+                        },
+                        gridLines: {
+                            color: "rgba(255,255,255, 0.5)",
+                        },
+                    },
+                ],
+                xAxes: [
+                    {
+                        ticks: {
+                            fontColor: "rgba(255,255,255, 0.5)",
+                        },
+                        gridLines: {
+                            color: "rgba(255,255,255, 0.5)",
                         },
                     },
                 ],
@@ -42,22 +64,44 @@ if (!gpuValues.lenght) {
             labels: h,
             datasets: [
                 {
-                    label: "Temperatura maior que 50° nos últimos 10 dias",
-                    data: gpuMoreThenFifty,
+                    label: "Temperaturas maiores que 50 nos últimos 10 dias",
+                    data: temps,
                     fill: false,
+                    color: "rgba(255,255,255, 0.5)",
+                    backgroundColor: "rgba(255,255,255, 0.5)",
+                    borderWidth: 1,
                     borderColor: "#9400D3",
                     tension: 0.1,
                 },
             ],
         },
         options: {
+            legend: {
+                labels: {
+                    fontColor: "rgba(255,255,255, 0.5)",
+                },
+            },
             scales: {
                 yAxes: [
                     {
                         ticks: {
+                            fontColor: "rgba(255,255,255, 0.5)",
                             beginAtZero: true,
                             max: 100,
                             min: 0,
+                        },
+                        gridLines: {
+                            color: "rgba(255,255,255, 0.5)",
+                        },
+                    },
+                ],
+                xAxes: [
+                    {
+                        ticks: {
+                            fontColor: "rgba(255,255,255, 0.5)",
+                        },
+                        gridLines: {
+                            color: "rgba(255,255,255, 0.5)",
                         },
                     },
                 ],
